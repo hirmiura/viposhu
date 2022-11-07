85cc178e161140b49bdc2705899038315e63cfcb
Hiroshi Miura <hirmiura@gmail.com>
2022-11-08 01:02:51 +0900

    refactor: 使っていないコードを削除した

--- viposhu/hoshu5ch_status.py
+++ viposhu/hoshu5ch_status.py
@@ -3,7 +3,7 @@
 # Copyright 2022 hirmiura <https://github.com/hirmiura>
 from dataclasses import dataclass
 from datetime import datetime, timedelta, timezone
-from typing import Any, Optional
+from typing import Optional
 
 JST = timezone(timedelta(hours=+9), "JST")
 
@@ -29,26 +29,6 @@ class Hoshu5chStatus:
 
     def update_last_post(self, now: Optional[datetime] = None) -> datetime:
         return self._update_X("last_post", now)
-
-    def _is_in_X(self, varname: str, num: int) -> bool:
-        assert num >= 0
-        dt: Optional[datetime] = None
-        exec_local: dict[Any, Any] = {"self": self, "dt": dt}
-        exec("dt = self.{0}".format(varname), globals(), exec_local)
-        dt = exec_local["dt"]
-        if not dt:
-            return False
-        now = datetime.now(JST)
-        delta = now - dt
-        if delta.total_seconds() < num:
-            return True
-        return False
-
-    def is_in_check_interval(self, interval_needed: int) -> bool:
-        return self._is_in_X("last_check", interval_needed)
-
-    def is_in_guard_time(self, guard_time_needed: int) -> bool:
-        return self._is_in_X("last_post", guard_time_needed)
 
     def get_interval_to_next_check(self, interval: int, guard_time: int) -> int:
         """次にチェックする時間を秒で返す
